c792977c0cb5ab4e6038bded1ba07fb249a5be9c
khwaja nawaz <khwajanawaz82@gmail.com>
2026-03-30 17:45:36 +0100

    infrastructure-main-eks-network-vpc-bootstrap

--- Infrastructure-main/0-bootstrap/main.tf
+++ Infrastructure-main/0-bootstrap/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = "productsShop-terraform-locks"
+  name         = "13microservices-eks-tf-state-khwajanawaz"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
