4c555744a257b5d72c01fc07d60af54b1d862478
khwaja nawaz <khwajanawaz82@gmail.com>
2026-03-30 19:56:22 +0100

    infrastructure-main-eks-network-vpc-bootstrap

--- Infrastructure-main/2-eks/data.tf
+++ Infrastructure-main/2-eks/data.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "productsShop-infra-statefile-backup"
-    key    = "productsShop/1-network/terraform.tfstate"
-    region = "ap-northeast-1"
+    bucket = "khwajanawaz-13microservices-tf-state-20260330"
+    key    = "1-network/terraform.tfstate"
+    region = "us-east-1"
   }
 }
