b541b5d5bde90c4d5af7ebc7a4b87d703411f30d
khwaja nawaz <khwajanawaz82@gmail.com>
2026-03-30 18:35:59 +0100

    infrastructure-main-eks-network-vpc-bootstrap

--- Infrastructure-main/1-network/backend.tf
+++ Infrastructure-main/1-network/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "khwajanawaz-13microservices-tf-state-20260330"
     key            = "khwajanawaz-13microservices/1-network/terraform.tfstate"
-    region         = "ap-northeast-1"
+    region         = "us-east-1"
     dynamodb_table = "khwajanawaz-13microservices-terraform-locks"
     encrypt        = true
   }
